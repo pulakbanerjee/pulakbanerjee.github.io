32bd36e7cd38f7aec6d3db7ab4f634ddeb463bad
pulakbanerjee <pulakbanerjee09@gmail.com>
2025-09-28 09:35:24 +0200

    Added a small code for a simple binary search of a sorted list.

---
+++ cpp/general/binary_search_simple.cpp
@@ -0,0 +1,54 @@
+// Simple binary search program to find if a sorted list contains a specific item. The code returns the number of itartations needed to find the number. It returns nothing, if the number is not found.
+// Pulak Banerjee, 28/09/2025
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+std::vector<int> myvect1;
+int start;
+int end;
+
+int item = 9;
+//int item = 110;
+myvect1={1,3,5,7,9,11,13,15,17,21,221,10010,34848,8393838,17163519};
+
+// print the vector
+
+//for (int y : myvect1)
+//        std::cout<<"print the vector: "<<y<<std::endl;
+
+int low = 0;
+int count=0;
+int high =  myvect1.size()-1;
+int mid=0;
+int guess=0;
+
+while(low<high)
+{
+  mid = (low+high);
+  guess = myvect1[mid];
+
+if(guess == item)
+{
+  cout<<"Number found in the list. Number of iterations needed are "<<count<<"."<<endl;
+break;
+}
+
+if (guess > item)
+{
+  high=mid-1;
+  count = count+1;
+
+}
+else
+{
+  low = mid+1;
+  count = count+1;
+}
+}
+
+return(0);
+}
